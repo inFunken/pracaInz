36709f5cb65ec5ea6b708384934144a6e1bbcb46
Piotr Sk <pitorusss21@gmail.com>
2017-12-16 21:44:56 +0100

    #comment added class for main window

---
+++ src/gui/Window.java
@@ -0,0 +1,19 @@
+package gui;
+
+import dbconnection.DBConnection;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Created by Piotr on 16.12.2017.
+ */
+public class Window extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Scene startingScene = new Scenes().startingScene(primaryStage);
+        primaryStage.setScene(startingScene);
+        DBConnection.connect();
+        primaryStage.show();
+    }
+}
