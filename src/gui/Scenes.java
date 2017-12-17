e454421974c61c07a7c939be22489d3d928a6808
Piotr Sk <pitorusss21@gmail.com>
2017-12-17 16:55:57 +0100

    #comment modified the starting scene

--- src/gui/Scenes.java
+++ src/gui/Scenes.java
@@ -1,10 +1,14 @@
 package gui;
 
 import graph.Nodes;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Scenes {
@@ -12,19 +16,55 @@ public class Scenes {
     public javafx.scene.Scene startingScene(Stage stage){
         StackPane stackPane = new StackPane();
         Scene startingScene = new Scene(stackPane, 1280, 720);
+        startingScene.getStylesheets().add("style.css");
+        VBox mainMenu = new VBox();
 
-
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction((ActionEvent event) -> {
-                System.out.println("Hello World!");
-                //DBConnection.getCityData();
+        Button btnNewGraph = new Button();
+        btnNewGraph.setPrefSize(250,30);
+        btnNewGraph.setText("Generate new graph");
+        btnNewGraph.setOnAction((ActionEvent event) -> {
+                System.out.println("Generate new graph");
                 Nodes.generateNodes(2);
                 stage.setScene(newGraphScene(stage));
         });
 
+        Button btnEditExistingGraph = new Button();
+        btnEditExistingGraph.setPrefSize(250,30);
+        btnEditExistingGraph.setText("Edit existing graph");
+        btnEditExistingGraph.setOnAction((ActionEvent event) -> {
+            System.out.println("Edit existing graph");
+            Nodes.generateNodes(2);
+            stage.setScene(newGraphScene(stage));
+        });
 
-        stackPane.getChildren().add(btn);
+        Button btnHelp = new Button();
+        btnHelp.setPrefSize(250,30);
+        btnHelp.setText("Help");
+        btnHelp.setOnAction((ActionEvent event) -> {
+            System.out.println("Help");
+            Nodes.generateNodes(2);
+            stage.setScene(newGraphScene(stage));
+        });
+
+        Button btnAboutAuthor = new Button();
+        btnAboutAuthor.setPrefSize(250,30);
+        btnAboutAuthor.setText("About author");
+        btnAboutAuthor.setOnAction((ActionEvent event) -> {
+            System.out.println("Info about author");
+            Nodes.generateNodes(2);
+            stage.setScene(newGraphScene(stage));
+        });
+
+        Button btnExit = new Button();
+        btnExit.setPrefSize(250,30);
+        btnExit.setText("Exit");
+        btnExit.setOnAction(e -> Platform.exit());
+
+        mainMenu.getChildren().addAll(btnNewGraph, btnEditExistingGraph, btnHelp, btnAboutAuthor, btnExit);
+        mainMenu.setAlignment(Pos.TOP_LEFT);
+        mainMenu.setPadding(new Insets(30, 30, 30, 30));
+        mainMenu.setSpacing(20);
+        stackPane.getChildren().add(mainMenu);
         return startingScene;
     }
 
@@ -36,7 +76,6 @@ public class Scenes {
         btn.setText("Say 'Hello World 2'");
         btn.setOnAction((ActionEvent event) -> {
             System.out.println("Hello World 2!");
-            //DBConnection.getCityData();
             Nodes.generateNodes(2);
             stage.setScene(startingScene(stage));
         });
