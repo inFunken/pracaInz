2f96e1968596ec2477ba74479c239bb3aa0c313d
Piotr Sk <pitorusss21@gmail.com>
2018-01-06 20:13:09 +0100

    #comment removed unnecessary lines

--- src/gui/Popups.java
+++ src/gui/Popups.java
@@ -7,9 +7,7 @@ public class Popups {
     public static void genericError(String contentText){
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("Error");
-        //alert.setHeaderText("Look, an Exception Dialog");
         alert.setContentText(contentText);
-
         alert.showAndWait();
     }
 
