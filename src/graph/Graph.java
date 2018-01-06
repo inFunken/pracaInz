57529e826d2d34382c320107aea2fade981a9bf0
Piotr Sk <pitorusss21@gmail.com>
2018-01-06 20:11:14 +0100

    #comment added graph pojo class to fill the existing graph table

---
+++ src/graph/Graph.java
@@ -0,0 +1,59 @@
+package graph;
+
+import java.sql.Date;
+
+/**
+ * Created by Piotr on 06.01.2018.
+ */
+public class Graph {
+    private int graphId;
+    private int nodeAmount;
+    private int connectionAmount;
+    private Date createDate;
+
+    public Graph() {
+        this.graphId = 0;
+        this.nodeAmount = 0;
+        this.connectionAmount = 0;
+        this.createDate = null;
+    }
+
+    public Graph(int graphId, int nodeAmount, int connectionAmount, Date createDate) {
+        this.graphId = graphId;
+        this.nodeAmount = nodeAmount;
+        this.connectionAmount = connectionAmount;
+        this.createDate = createDate;
+    }
+
+    public int getGraphId() {
+        return graphId;
+    }
+
+    public void setGraphId(int graphId) {
+        this.graphId = graphId;
+    }
+
+    public int getNodeAmount() {
+        return nodeAmount;
+    }
+
+    public void setNodeAmount(int nodeAmount) {
+        this.nodeAmount = nodeAmount;
+    }
+
+    public int getConnectionAmount() {
+        return connectionAmount;
+    }
+
+    public void setConnectionAmount(int connectionAmount) {
+        this.connectionAmount = connectionAmount;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+}
