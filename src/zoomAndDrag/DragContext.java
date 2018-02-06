77b0f5b3e8b8cb7d6ac6236ee9bd83b4525663a5
Piotr Sk <pitorusss21@gmail.com>
2018-02-07 00:27:45 +0100

    Added package with dragging and zooming utility

---
+++ src/zoomAndDrag/DragContext.java
@@ -0,0 +1,14 @@
+package zoomAndDrag;
+
+/**
+ * Created by Piotr on 05.02.2018.
+ */
+class DragContext {
+
+    double mouseAnchorX;
+    double mouseAnchorY;
+
+    double translateAnchorX;
+    double translateAnchorY;
+
+}
---
+++ src/zoomAndDrag/NodesOnCircles.java
@@ -0,0 +1,41 @@
+package zoomAndDrag;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * Created by Piotr on 05.02.2018.
+ */
+public class NodesOnCircles {
+    final double x;
+    final double y;
+
+    public NodesOnCircles(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public static NodesOnCircles generateRandomNodes(double startingX, double startingY, double radius){
+        double x, y, angle, random;
+        int randomSign = 0;
+
+        angle = Math.random()*Math.PI;
+
+        random = ThreadLocalRandom.current().nextDouble(-1, 1 +1);
+
+        while (randomSign == 0)
+            randomSign = ThreadLocalRandom.current().nextInt(-1, 1 +1);
+
+        x = startingX + randomSign * (Math.cos(angle) * radius) + random;
+        y = startingY + randomSign * (Math.sin(angle) * radius) + random;
+
+        return new NodesOnCircles(x,y);
+    }
+}
