92c7c196c646f45e56b51a4778268b7068b85ff3
Piotr Sk <pitorusss21@gmail.com>
2018-04-01 18:55:27 +0200

    Removed unnecessary lines

--- src/gui/Popups.java
+++ src/gui/Popups.java
@@ -3,13 +3,10 @@ package gui;
 import javafx.scene.control.Alert;
 
 public class Popups {
-
     public static void genericError(String contentText){
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("Error");
         alert.setContentText(contentText);
         alert.showAndWait();
     }
-
 }
-
