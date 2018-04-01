9b403fdffdf7d34956da817f6c6e8bb7d15cfdf9
Piotr Sk <pitorusss21@gmail.com>
2018-04-01 18:59:36 +0200

    Removed create comment and other comments

--- src/zoomAndDrag/PannableCanvas.java
+++ src/zoomAndDrag/PannableCanvas.java
@@ -2,75 +2,31 @@ package zoomAndDrag;
 
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-
-/**
- * Created by Piotr on 11.01.2018.
- */
 
 public class PannableCanvas extends Pane {
 
-
-    DoubleProperty myScale = new SimpleDoubleProperty(1.0);
+    DoubleProperty scale = new SimpleDoubleProperty(1.0);
 
     public PannableCanvas() {
         setPrefSize(600, 600);
         setStyle("-fx-background-color: white; -fx-border-color: blue;");
 
-        // add scale transform
-        scaleXProperty().bind(myScale);
-        scaleYProperty().bind(myScale);
-    }
-
-    /**
-     * Add a grid to the canvas, send it to back
-     */
-    public void addGrid() {
-
-        double w = getBoundsInLocal().getWidth();
-        double h = getBoundsInLocal().getHeight();
-
-        // add grid
-        Canvas grid = new Canvas(w, h);
-
-        // don't catch mouse events
-        grid.setMouseTransparent(true);
-
-        GraphicsContext gc = grid.getGraphicsContext2D();
-
-        gc.setStroke(Color.WHITE);
-        gc.setLineWidth(1);
-
-        // draw grid lines
-        double offset = 50;
-        for( double i=offset; i < w; i+=offset) {
-            gc.strokeLine( i, 0, i, h);
-            gc.strokeLine( 0, i, w, i);
-        }
-
-        getChildren().add( grid);
-
-        grid.toBack();
+        scaleXProperty().bind(scale);
+        scaleYProperty().bind(scale);
     }
 
     public double getScale() {
-        return myScale.get();
+        return scale.get();
     }
 
-    public void setScale( double scale) {
-        myScale.set(scale);
+    public void setScale( double setScale) {
+        scale.set(setScale);
     }
 
     public void setPivot( double x, double y) {
         setTranslateX(getTranslateX()-x);
         setTranslateY(getTranslateY()-y);
     }
-
-
-
-
 }
 
