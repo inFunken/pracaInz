ffcd8677c6129a45de69d13285bf3ff53422db62
Piotr Sk <pitorusss21@gmail.com>
2018-04-01 18:57:27 +0200

    Removed create comment and blank lines

--- src/zoomAndDrag/DragScene.java
+++ src/zoomAndDrag/DragScene.java
@@ -1,14 +1,8 @@
 package zoomAndDrag;
 
-/**
- * Created by Piotr on 05.02.2018.
- */
-class DragContext {
-
+class DragScene {
     double mouseAnchorX;
     double mouseAnchorY;
-
     double translateAnchorX;
     double translateAnchorY;
-
 }
